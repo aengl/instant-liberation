3692370c8b4d66402b80abde7b54145935b32a58
Lynn Smeria <ae@cephea.de>
2018-09-10 15:21:12 +0200

    Support private accounts

--- src/crawler.js
+++ src/crawler.js
@@ -1,11 +1,10 @@
+const _ = require('lodash');
 const debug = require('debug')('instalib:crawler');
 const fs = require('fs-extra');
-const _ = require('lodash');
-const axios = require('axios');
-const path = require('path');
 const os = require('os');
-const yaml = require('js-yaml');
+const path = require('path');
 const puppeteer = require('puppeteer');
+const yaml = require('js-yaml');
 
 const defaultDataRoot = path.resolve(os.homedir(), '.instalib');
 
@@ -25,12 +24,13 @@ module.exports = {
       mediaRoot: path.resolve('media'),
       dataRoot: defaultDataRoot,
     });
+
+    // Go to user page
+    debug(`navigating to "${url}"`);
     const browser = await puppeteer.launch({
       userDataDir: options.dataRoot,
     });
     const page = await browser.newPage();
-
-    // Go to user page
     const response = await page.goto(url);
     if (!response.ok()) {
       throw new Error(response.status());
@@ -42,19 +42,33 @@ module.exports = {
       .map(href => href.match(/\/p\/(?<id>[^/]+)\//))
       .filter(match => !!match)
       .map(match => match.groups.id);
-    debug(`found ${postIDs.length} posts:`);
-    debug(postIDs);
+    debug(`found ${postIDs.length} post(s)`);
 
-    await page.close();
-    await browser.close();
+    if (postIDs.length === 0) {
+      debug('the account might be private -- try calling "login" first');
+    } else {
+      // Get GraphQL data for all posts
+      debug(`querying data for all posts`);
+      const results = await Promise.all(
+        postIDs.map(id =>
+          page.evaluate(async _id => {
+            // eslint-disable-next-line no-undef
+            const res = await fetch(
+              `https://www.instagram.com/p/${_id}/?__a=1`
+            );
+            const json = await res.json();
+            return json;
+          }, id)
+        )
+      );
+      const data = results.map(result => result.graphql.shortcode_media);
 
-    // Get GraphQL data for all posts
-    const results = await Promise.all(
-      postIDs.map(id => axios.get(`https://www.instagram.com/p/${id}/?__a=1`))
-    );
-    const data = results.map(result => result.data.graphql.shortcode_media);
+      await page.close();
+      await browser.close();
 
-    // Store data
-    fs.writeFileSync(options.dataPath, yaml.dump(data));
+      // Store data
+      debug(`writing data to "${options.dataPath}"`);
+      fs.writeFileSync(options.dataPath, yaml.dump(data));
+    }
   },
 };
